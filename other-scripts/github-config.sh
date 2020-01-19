68dc33eeb302c95e54a780704a4a3e74a9a0fc8b
Daniel Silva <daniel@silvanolte.com>
2020-01-19 18:24:45 -0500

    Added github-config.sh script.

---
+++ other-scripts/github-config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script for setting up Github. Edit script with your details before use.
+
+echo "EDIT SCRIPT BEFORE USE"
+
+# ssh-keygen -t rsa -b 4096 -C "YOUR_EMAIL"
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_rsa
+# git config --global user.name "YOUR_NAME"
+# git config --global user.email "YOUR_EMAIL"
+
+# echo "
+
+# Now add ~/.ssh/id_rsa.pub to your Github account and verify with the following command:
+
+#     ssh -T git@github.com"
