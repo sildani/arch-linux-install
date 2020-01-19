6be8c5ee47ab86d57c35e15508224015f1f89bb4
Daniel Silva <daniel@silvanolte.com>
2020-01-19 15:47:08 -0500

    Fixed issue in 02 script.

--- 02-install-linux.sh
+++ 02-install-linux.sh
@@ -11,6 +11,7 @@ pacstrap /mnt base linux linux-firmware vim
 # generate file system table on new disk, and chroot into the new file system
 genfstab -U /mnt >> /mnt/etc/fstab
 cp ~/arch-linux-install/03-config-linux.sh /mnt
-cp ~/arch-linux-install/05-complete-user-setup-with-ui.sh /mnt
+cp ~/arch-linux-install/05-user-setup.sh /mnt
+cp ~/arch-linux-install/06-ui-setup.sh /mnt
 cp -R ~/arch-linux-install/other-scripts /mnt/
 arch-chroot /mnt
