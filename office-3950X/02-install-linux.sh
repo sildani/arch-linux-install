a80b3ea2e48d9d0a33c3b37380089531951f6508
Daniel Silva <daniel@silvanolte.com>
2020-01-19 21:25:07 -0500

    Added back amd-ucode package to my personal scripts

---
+++ office-3950X/02-install-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# edit mirror list to move US mirrors to top of list
+pacman -S reflector
+sudo reflector --country "United States" --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+# install Arch Linux onto disk + other base packages
+# add amd-ucode or intel-ucode if not running on a vm
+pacstrap /mnt base linux linux-firmware vim sudo amd-ucode
+
+# generate file system table on new disk, and chroot into the new file system
+genfstab -U /mnt >> /mnt/etc/fstab
+cp ~/arch-linux-install/03-config-linux.sh /mnt
+cp ~/arch-linux-install/05-user-setup.sh /mnt
+cp ~/arch-linux-install/06-ui-setup.sh /mnt
+cp -R ~/arch-linux-install/other-scripts /mnt/
+arch-chroot /mnt
--- office-3950X/stage-scripts.sh
+++ office-3950X/stage-scripts.sh
@@ -4,5 +4,7 @@
 
 mv ./01-partition-disk.sh ./01-partition-disk.sh.orig && chmod -x ./01-partition-disk.sh.orig
 cp ./office-3950X/01-partition-disk.sh .
+mv ./02-install-linux.sh ./02-install-linux.sh.orig && chmod -x ./02-install-linux.sh.orig
+cp ./office-3950X/02-install-linux.sh .
 mv ./06-ui-setup.sh ./06-ui-setup.sh.orig && chmod -x ./06-ui-setup.sh.orig
 cp ./office-3950X/06-ui-setup.sh .
