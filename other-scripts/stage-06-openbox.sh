8677e2187ab963a44fec110960a6510044c2330e
Daniel Silva <daniel@silvanolte.com>
2020-01-22 08:44:00 -0500

    Helper script to accelerate testing cycle.

---
+++ other-scripts/stage-06-openbox.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/arch-linux-install && git pull --rebase && cd
+cp ~/arch-linux-install/resources/* ~/
+sed 's/amdgpu/vmware/' ~/arch-linux-install/office-3950X/06-openbox-setup.sh > ~/bin/06-openbox-setup.sh
+chmod +x ~/bin/06-openbox-setup.sh
