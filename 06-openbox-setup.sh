7d966b5ead0e850fe534be21965fb6e1606ee883
Daniel Silva <daniel@silvanolte.com>
2020-01-22 16:23:56 -0500

    Moved openbox setup script out of office 3950x dir going to make it more specific for VMs and take a different approach on how to stage scripts for my home computer.

--- 06-openbox-setup.sh
+++ 06-openbox-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# required packages for script (assumes amd gpu)
-sudo pacman -Sy xorg-server openbox obconf nitrogen tint2 arc-gtk-theme breeze lxappearance pcmanfm archlinux-xdg-menu lightdm lightdm-gtk-greeter xdg-user-dirs numlockx archlinux-wallpaper xf86-video-amdgpu code tilix htop
+# required packages for script
+sudo pacman -Sy xorg-server openbox obconf nitrogen tint2 arc-gtk-theme breeze lxappearance pcmanfm archlinux-xdg-menu lightdm lightdm-gtk-greeter xdg-user-dirs numlockx archlinux-wallpaper xf86-video-vmware code tilix htop
 
 # setup display manager
 systemctl enable lightdm.service
@@ -49,8 +49,8 @@ cp ~/.config/tint2/minima/minima.tint2rc ~/.config/tint2/tint2rc
 
 # TODO - theme gtk and qt windows (breeze and lxappearance)
 # TODO - automate the staging of these files
-mkdir -p ~/.config/gtk-2.0 && mv ~/gtk-2-gtkfilechooser.ini ~/.config/gtk-2.0/gtkfilechooser.ini
-mkdir -p ~/.config/gtk-3.0 && mv ~/gtk-3-settings.ini ~/.config/gtk-3.0/settings.ini
+# mkdir -p ~/.config/gtk-2.0 && mv ~/gtk-2-gtkfilechooser.ini ~/.config/gtk-2.0/gtkfilechooser.ini
+# mkdir -p ~/.config/gtk-3.0 && mv ~/gtk-3-settings.ini ~/.config/gtk-3.0/settings.ini
 
 # desktop wallpaper manager (nitrogen)
 # TODO - automate the staging of these files
