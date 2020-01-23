10f6fc3368f3a3e045ed50f1f1534511535eb3b9
Daniel Silva <daniel@silvanolte.com>
2020-01-22 21:34:47 -0500

    Add my wallpapers to the install, and select the gryphon rider as the default.

--- other-scripts/stage-06-openbox.sh
+++ other-scripts/stage-06-openbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/arch-linux-install && git pull --rebase && cd
-cp ~/arch-linux-install/resources/* ~/
+cp -R ~/arch-linux-install/resources/* ~/
 #sed 's/amdgpu/vmware/' ~/arch-linux-install/office-3950X/06-openbox-setup.sh > ~/bin/06-openbox-setup.sh
 cp ~/arch-linux-install/06-openbox-setup.sh ~/bin/06-openbox-setup.sh
 chmod +x ~/bin/06-openbox-setup.sh
