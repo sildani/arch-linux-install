def7cfac563f4b6fc48baae135abc3d8273e1369
Daniel Silva <daniel@silvanolte.com>
2020-01-24 09:55:54 -0500

    Re-orgnized scripts.

--- other-scripts/stage-06-openbox.sh
+++ other-scripts/stage-06-openbox.sh
@@ -3,5 +3,5 @@
 cd ~/arch-linux-install && git pull --rebase && cd
 cp -R ~/arch-linux-install/resources/* ~/
 #sed 's/amdgpu/vmware/' ~/arch-linux-install/office-3950X/06-openbox-setup.sh > ~/bin/06-openbox-setup.sh
-cp ~/arch-linux-install/06-openbox-setup.sh ~/bin/06-openbox-setup.sh
+cp ~/arch-linux-install/other-scripts/06-openbox-setup.sh ~/bin/06-openbox-setup.sh
 chmod +x ~/bin/06-openbox-setup.sh
