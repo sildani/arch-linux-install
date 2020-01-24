5350d68e09601b8838ed3f89ea643d587ea82ae6
Daniel Silva <daniel@silvanolte.com>
2020-01-24 11:05:39 -0500

    corrected pathing in staging script for office-3950x

--- office-3950X/stage-scripts.sh
+++ office-3950X/stage-scripts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # stage disk partitioning
-cp ~/arch-linux-install/office-3950X/01-partition-disk.sh ~/01-partition-disk.sh
+cp ~/arch-linux-install/office-3950X/01-partition-disk.sh ~/arch-linux-install/01-partition-disk.sh
 
 # stage linux install
 sed 's/pacstrap \/mnt base linux linux-firmware vim sudo/pacstrap \/mnt base linux linux-firmware vim sudo amd-ucode/' ~/arch-linux-install/02-install-linux.sh > /tmp/02-install-linux.sh
 mv /tmp/02-install-linux.sh ~/arch-linux-install/02-install-linux.sh
 
 # stage ui setup
-cp ~/arch-linux-install/office-3950X/06-ui-setup.sh ~/06-ui-setup.sh
+cp ~/arch-linux-install/office-3950X/06-ui-setup.sh ~/arch-linux-install/06-ui-setup.sh
