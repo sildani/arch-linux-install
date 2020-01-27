4848503882b6959292970da88d352dfbb56aff1e
Daniel Silva <daniel@silvanolte.com>
2020-01-27 10:11:49 -0500

    Initial i3 staging and setup scripts.

---
+++ other-scripts/i3/06-i3-setup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# required packages for script
+sudo pacman -Sy --noconfirm \
+xorg-server lightdm lightdm-gtk-greeter xdg-user-dirs numlockx archlinux-wallpaper \
+xf86-video-vmware alsa alsa-utils pulseaudio bluez bluez-utils code tilix htop \
+i3-wm
+
+# setup display manager
+sudo systemctl enable lightdm.service
+echo "
+[Seat:*]
+greeter-setup-script=/usr/bin/numlockx on" >> /tmp/numlock.tmp
+sudo bash -c 'cat /tmp/numlock.tmp >> /etc/lightdm/lightdm.conf'
+rm /tmp/numlock.tmp
+# TODO - configure lightdm background
+
+# create user dirs
+xdg-user-dirs-update
+sudo pacman -Rn --noconfirm xdg-user-dirs
+
+# setup bluetooth
+sudo systemctl enable bluetooth.service
+
+# setup terminal
+echo "
+if [ \$TILIX_ID ] || [ \$VTE_VERSION ]; then
+  source /etc/profile.d/vte.sh
+fi" >> ~/.zshrc
+dconf write /com/gexperts/Tilix/terminal-title-show-when-single false
+dconf write /com/gexperts/Tilix/terminal-title-style "'small'"
+dconf write /com/gexperts/Tilix/theme-variant "'dark'"
+
+# enable multi-processor package building (in preparation for building and installing AURs)
+sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j4"/' /etc/makepkg.conf
+
+# install Chrome AUR
+yay -aS --noconfirm --answerdiff=None google-chrome
+ln -s /usr/bin/google-chrome-stable ~/bin/chrome
+
+# add readme to desktop
+echo "From here on out, you can set up your system however you want it.
+
+Some things I like to install / configure:
+
+- Look and feel
+  (desktop, taskbar, launcher, themes, fonts)
+- ACPI event controls
+- Gaming support (Wine, 3D Drivers, Lutris)
+- Clipboard manager
+
+See ~/bin/other-scripts for some options. Enjoy!" >> ~/Desktop/README.md
+
+# i3 stuff goes here
+
+# clean up
+rm ~/06-setup-gui.sh
+
+# prompt user to reboot
+echo "#######################################
+#                                     #
+#  Type \`reboot\` to reboot now and    #
+#  launch your new UI. :)             #
+#                                     #
+#######################################"
---
+++ other-scripts/i3/stage-i3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ~/arch-linux-install/other-scripts/i3/06-i3-setup.sh ~/arch-linux-install/06-setup-gui.sh
