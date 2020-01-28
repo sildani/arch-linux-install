0735f61760d59065c4dc63cb2b93120359889495
Daniel Silva <daniel@silvanolte.com>
2020-01-27 21:13:52 -0500

    Added a series of packages to i3 setup.

--- other-scripts/i3/06-i3-setup.sh
+++ other-scripts/i3/06-i3-setup.sh
@@ -4,7 +4,7 @@
 sudo pacman -Sy --noconfirm \
 xorg-server lightdm lightdm-gtk-greeter xdg-user-dirs numlockx archlinux-wallpaper \
 xf86-video-vmware alsa alsa-utils pulseaudio bluez bluez-utils code tilix htop \
-i3-wm i3status nitrogen dmenu pasystray
+i3-wm i3status i3lock nitrogen dmenu pasystray arc-gtk-theme breeze lxappearance pcmanfm lxrandr
 
 # setup display manager
 sudo systemctl enable lightdm.service
