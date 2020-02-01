89f64239f4c7d8d5c6db210e21298de935cbe4e1
Daniel Silva <daniel@silvanolte.com>
2020-02-01 05:30:10 -0500

    Rearranged order of commands, according to app.

--- other-os/manjaro-i3/after-manjaro-i3-install.sh
+++ other-os/manjaro-i3/after-manjaro-i3-install.sh
@@ -45,6 +45,7 @@ echo "
 if [ \$TILIX_ID ] || [ \$VTE_VERSION ]; then
   source /etc/profile.d/vte.sh
 fi" >> ~/.zshrc
+echo "export TERMINAL=/usr/bin/tilix" >> ~/.profile
 dconf write /com/gexperts/Tilix/terminal-title-show-when-single false
 dconf write /com/gexperts/Tilix/terminal-title-style "'small'"
 dconf write /com/gexperts/Tilix/theme-variant "'dark'"
@@ -58,6 +59,9 @@ cd ~/
 
 # install chrome
 yay -aS --noconfirm --needed --answerdiff=None google-chrome
+sed -i 's/Pale Moon.desktop/google-chome.desktop/g' ~/.config/mimeapps.list
+sed -i 's/palemoon/google-chrome-stable/g' ~/.profile
+sed -i 's/palemoon/google-chrome-stable/g' ~/.i3/config
 
 # install visual studio code
 yay -aS --noconfirm --needed --answerdiff=None visual-studio-code-bin
@@ -83,14 +87,10 @@ sed -i 's/\/usr\/share\/conky/~\/.conky/g' ~/bin/start_conky_maia
 sed -i 's/gap_y = 13/gap_y = 50/g' ~/.conky/conky_maia
 
 # setup i3
-sed -i 's/palemoon/google-chrome-stable/g' ~/.i3/config
 sed -i 's/\# exec --no-startup-id blueman-applet/exec --no-startup-id blueman-applet/g' ~/.i3/config
 sed -i 's/position bottom/position top/g' ~/.i3/config
 sed -i 's/bindsym \$mod+Return exec terminal/bindsym \$mod+Return exec i3-sensible-terminal/g' ~/.i3/config
 sed -i 's/start_conky_maia/~\/bin\/start_conky_maia/g' ~/.i3/config
-sed -i 's/Pale Moon.desktop/google-chome.desktop/g' ~/.config/mimeapps.list
-sed -i 's/palemoon/google-chrome-stable/g' ~/.profile
-echo "export TERMINAL=/usr/bin/tilix" >> ~/.profile
 
 # setup i3status
 cp ~/code/arch-linux-install/other-os/manjaro-i3/resources/i3status.conf ~/.i3status.conf
