df06487d742276bbdc7554d49e8d67ea87434b07
Daniel Silva <daniel@silvanolte.com>
2020-02-28 15:11:27 -0500

    Morc Menu setup

--- other-os/salient-os/after-salient-os-install.sh
+++ other-os/salient-os/after-salient-os-install.sh
@@ -3,8 +3,16 @@
 # set up zsh
 chsh -s /bin/zsh
 cp /etc/skel/.zshrc ~/.zshrc
-sudo pacman -Sy zsh-theme-powerlevel9k
+sudo pacman -Sy --noconfirm zsh-theme-powerlevel9k
 echo "
 powerline-daemon -q
 source /usr/lib/python3.8/site-packages/powerline/bindings/zsh/powerline.zsh
 source /usr/share/zsh-theme-powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
+
+# set up morc_menu
+git clone https://github.com/boruch-baum/morc_menu ~/code/morc_menu
+cd ~/code/morc_menu
+sudo make install
+echo "
+bindsym \$mod+z exec --no-startup-id morc_menu" >> ~/.config/i3/config
+cp ~/code/arch-linux-install/other-os/salient-os/resources/morc_menu_v1.conf ~/.config/morc_menu/
