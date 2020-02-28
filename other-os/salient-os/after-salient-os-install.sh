8e5b185cf855c5389e384ae168b183520614f1c0
Daniel Silva <daniel@silvanolte.com>
2020-02-28 15:45:39 -0500

    added alsi to zshrc

--- other-os/salient-os/after-salient-os-install.sh
+++ other-os/salient-os/after-salient-os-install.sh
@@ -17,3 +17,6 @@ echo "
 bindsym \$mod+z exec --no-startup-id morc_menu" >> ~/.config/i3/config
 mkdir -p ~/.config/morc_menu
 cp ~/code/arch-linux-install/other-os/salient-os/resources/morc_menu_v1.conf ~/.config/morc_menu/
+
+# setup alsi on new shell
+echo "alsi -l" >> ~/.zshrc
