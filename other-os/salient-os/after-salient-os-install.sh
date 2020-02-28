ccd932462dd1fdb726e2c173dd0d8646f24c8b94
Daniel Silva <daniel@silvanolte.com>
2020-02-28 09:53:20 -0500

    Created salient os script with initial zsh setup

---
+++ other-os/salient-os/after-salient-os-install.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+
+# set up zsh
+chsh -s /bin/zsh
+cp /etc/skel/.zshrc ~/.zshrc
+sudo pacman -Sy zsh-theme-powerlevel9k
+echo "
+powerline-daemon -q
+source /usr/lib/python3.8/site-packages/powerline/bindings/zsh/powerline.zsh
+source /usr/share/zsh-theme-powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
