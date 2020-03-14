785999c7244c484743974c2b623f55ba214dd29a
Daniel Silva <daniel@silvanolte.com>
2020-03-14 14:06:50 -0400

    salient os add bluetooth config

--- other-os/salient-os/after-salient-os-install.sh
+++ other-os/salient-os/after-salient-os-install.sh
@@ -19,6 +19,12 @@ sed -i 's/chromium/vivaldi-stable/g' ~/.config/i3/config
 # install visual studio code
 yay -aS --noconfirm --needed --answerdiff=None visual-studio-code-bin
 
+# install blueman (bluetooth gui), setup bluetooth, and update grub
+yay -S --noconfirm --needed blueman
+sudo sed -i 's/#AutoEnable=false/AutoEnable=true/g' /etc/bluetooth/main.conf
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet logo.nologo acpi_osi=Linux video.use_native_backlight=1 audit=0"/GRUB_CMDLINE_LINUX_DEFAULT="quiet logo.nologo acpi_osi=Linux video.use_native_backlight=1 audit=0 bluetooth.disable_ertm=1"/g' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
 # install emoji font
 sudo pacman -S --noconfirm noto-fonts-emoji
 sudo cp ~/code/arch-linux-install/other-os/salient-os/resources/fonts-local.conf /etc/fonts/local.conf
