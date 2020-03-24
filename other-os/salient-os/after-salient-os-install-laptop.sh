71d9b51dfc3d76f3383dc4d7c32d20cbe5a19f22
Daniel Silva <daniel@silvanolte.com>
2020-03-23 21:33:17 -0400

    salient os put lid closing behavior statements together

--- other-os/salient-os/after-salient-os-install-laptop.sh
+++ other-os/salient-os/after-salient-os-install-laptop.sh
@@ -9,6 +9,8 @@ cp ~/code/arch-linux-install/other-os/salient-os/resources/laptop-polybar-config
 # setup lid closing behavior
 sudo cp /etc/systemd/logind.conf /etc/systemd/logind.conf.orig
 sudo cp ~/code/arch-linux-install/other-os/salient-os/resources/laptop-logind.conf /etc/systemd/logind.conf
+sudo cp ~/code/arch-linux-install/other-os/salient-os/resources/lock.service /etc/systemd/system/lock.service
+sudo systemctl enable lock.service
 
 # fix dpms settings (set display to go to sleep after 5 minutes)
 sed -i 's/#exec --no-startup-id xset dpms 0 0 1200/exec --no-startup-id xset dpms 0 0 300/g' ~/.config/i3/config
@@ -26,7 +28,3 @@ echo "
 #-------------------------------------------------------------------------
 bindsym XF86MonBrightnessDown exec --no-startup-id light -U 10
 bindsym XF86MonBrightnessUp exec --no-startup-id light -A 10"  >> ~/.config/i3/config
-
-# set up lock on suspend
-sudo cp ~/code/arch-linux-install/other-os/salient-os/resources/lock.service /etc/systemd/system/lock.service
-sudo systemctl enable lock.service
