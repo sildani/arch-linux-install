88f96f3c21e413b3caa116913eec73ec5edd9d01
Daniel Silva <daniel@silvanolte.com>
2020-03-25 07:33:37 -0400

    salient os add natural scrolling cue for laptop setup

--- other-os/salient-os/after-salient-os-install-laptop.sh
+++ other-os/salient-os/after-salient-os-install-laptop.sh
@@ -18,6 +18,16 @@ sed -i 's/#exec --no-startup-id xset dpms 0 0 1200/exec --no-startup-id xset dpm
 # set max window size for floating windows
 sed -i 's/floating_maximum_size -1 x -1/floating_maximum_size 1600 x 900/g' ~/.config/i3/config
 
+# reverse mouse wheel scroll
+echo -n "
+For natural (reverse) scrolling, add the following to /usr/share/X11/xorg.conf.d/40-libinput.conf, for the touchpad InputClass section:
+
+        Option \"NaturalScrolling\" \"True\"
+"
+read -p "Copy the above to the clipboard and press any key to edit the file in vim... "
+sudo vim /usr/share/X11/xorg.conf.d/40-libinput.conf
+echo "DONE"
+
 # set up screen brightness keybinds
 sudo usermod -a -G video daniel
 yay -S --noconfirm light
