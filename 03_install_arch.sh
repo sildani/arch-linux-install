2041b5fc4752ba6ac3ebc74a2f159c0c7e310e49
Daniel Silva <daniel@silvanolte.com>
2020-04-09 14:11:37 -0400

    more updates

--- 03_install_arch.sh
+++ 03_install_arch.sh
@@ -204,9 +204,7 @@ yay -Sy --noconfirm zsh-theme-powerlevel10k
 echo "
 # powerlevel10k theme
 source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
-POWERLEVEL10K_RIGHT_PROMPT_ELEMENTS=()
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc
+POWERLEVEL10K_RIGHT_PROMPT_ELEMENTS=()" >> ~/.zshrc
 
 # install htop
 yay -Sy --noconfirm htop
