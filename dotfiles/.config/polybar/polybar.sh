6f4af8e412e0fe61792393def08b5b223e941e01
Daniel Silva <daniel@silvanolte.com>
2020-04-11 21:18:16 -0400

    fixed polybar confirm, renamed bar

--- dotfiles/.config/polybar/polybar.sh
+++ dotfiles/.config/polybar/polybar.sh
@@ -9,24 +9,26 @@
 
 killall -q polybar &
 
-listpid=$(getpid "i3listen.py")
+listpid=$(pidof "i3listen.py")
 [[ -n $listpid ]] && kill "$listpid"
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done &
 
-i3fehrb > /dev/null 2>&1 &
+polybar -c ~/.config/polybar/config mybar --reload &
+
+# i3fehrb > /dev/null 2>&1 &
 
 #. $HOME/.fehbg
 
-MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+# MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
 
-if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d ":" -f1); do
-    	MONITOR=$m polybar -c ~/.config/polybar/config --reload example &
-	done
-else
-	polybar -c ~/.config/polybar/config example --reload &
-fi
+# if type "xrandr"; then
+# 	for m in $(xrandr --query | grep " connected" | cut -d ":" -f1); do
+#     	MONITOR=$m polybar -c ~/.config/polybar/config --reload example &
+# 	done
+# else
+# 	polybar -c ~/.config/polybar/config example --reload &
+# fi
 
 # start listener
 i3listen.py > /dev/null 2>&1 &
