832135aa78bf3d385e880b921587dc91e42b4b14
Daniel Silva <daniel@silvanolte.com>
2020-04-17 23:09:45 -0400

    refactored setting audio settings in i3 to an after-install script

---
+++ resources/after-install-scripts/add_audio_setup_to_i3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# creates a script that can be invoked from i3 to set defaults for audio devices
+
+ali_current_user=`whoami`
+ali_set_audio_script="/home/$ali_current_user/bin/set_default_sound_settings.sh"
+
+cat << 'EOF' > $ali_set_audio_script
+#!/bin/bash
+
+# use "aplay -l" command to get a list of card IDs
+# use "alsamixer" to verify settings / controls
+
+amixer set Master 50%
+sleep 1
+amixer -c `aplay -l | grep HD-Audio | head -1 | cut -b 5-6` set Headphone 100
+sleep 1
+amixer -c `aplay -l | grep HD-Audio | head -1 | cut -b 5-6` set PCM 90%
+sleep 1
+amixer -c `aplay -l | grep HD-Audio | head -1 | cut -b 5-6` set Front 80
+sleep 1
+amixer -c `aplay -l | grep HD-Audio | head -1 | cut -b 5-6` set Master 63
+sleep 1
+amixer -c `aplay -l | grep USB2.0 | head -1 | cut -b 5-6` set PCM 80%
+EOF
+
+chmod +x $ali_set_audio_script
+
+echo "
+# audio setup
+exec_always --no-startup-id $ali_set_audio_script" >> ~/.config/i3/config
