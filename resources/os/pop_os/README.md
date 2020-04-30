6011008f405edd8fb49d224f149b2a3993c568b3
Daniel Silva <daniel@silvanolte.com>
2020-04-30 11:26:19 -0400

    added a space to put post POP_OS\! scripts

---
+++ resources/os/pop_os/README.md
@@ -0,0 +1,2 @@
+Script to run after Pop_OS! is installed to configure system to my tastes.
+
---
+++ resources/os/pop_os/pop_os_post_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO: Enable window tiling
+# TODO: Enable 24 hour clock
+# TODO: Enable reverse trackpad scrolling
+# TODO: Update the system
+# TODO: Install Vim
+# TODO: Install Visual Studio Code
+# TODO: Install Vivaldi
+# TODO: Setup Zshrc and Powerlevel10k
+# TODO: Setup git global config
+# TODO: Setup github:sildani/scripts in ~/bin
+# TODO: Restore backed up data
